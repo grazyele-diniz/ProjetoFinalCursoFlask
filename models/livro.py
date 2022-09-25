abc8140d53fc3195ffcc2da7040ec9d11946da3d
Grazyele Diniz <grazyele.diniz@gmail.com>
2022-09-25 17:41:40 -0300

    modificando o arquivo livro.py

---
+++ models/livro.py
@@ -0,0 +1,12 @@
+from ..extensions import db
+
+class Livro(db.Model):
+    __tablename__ = "livros"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    titulo = db.Column(db.String(150))
+    vinculo = db.Column(db.String(50))
+    emprestimo = db.Column(db.Date)
+    devolucao = db.Column(db.Date)
+
+    def __repr__(self):
+        return "<Uc(titulo={}, vinculo={}, emprestimo={}, devolucao={})>".format(self.titulo, self.vinculo, self.emprestimo, self.devolucao)
